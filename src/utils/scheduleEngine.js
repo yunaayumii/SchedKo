b47789ecbdc2cdca7715326b7e87a3e0c55dea9a
Yuan Miguel Rubio <yuanmiguelrubio@gmail.com>
2026-07-29 09:37:35 +0800

    refactor: add and move files to src/utils folder

--- src/utils/scheduleEngine.js
+++ src/utils/scheduleEngine.js
@@ -3,9 +3,7 @@ function doPeriodsOverlap(periodA, periodB) {
     // 1. If they aren't on the same day, they can NEVER overlap
     if (periodA.day !== periodB.day) return false;
 
-    // 2. Return true if time ranges overlap:
-    // Hint: Two intervals [startA, endA] and [startB, endB] overlap if 
-    // startA < endB AND startB < endA
+    // true if time ranges overlap:
     return ((periodA.endTime > periodB.startTime) && (periodA.startTime < periodB.endTime))
 }
 
@@ -21,12 +19,6 @@ function doSectionsOverlap(sectionA, sectionB) {
     return false;
 }
 
-/**
- * Checks if a candidate section can fit into an existing schedule without conflicts.
- * @param {Object} candidateSection - The section object we want to test
- * @param {Array<Object>} currentCombination - Array of section objects already picked
- * @returns {boolean} True if candidateSection does NOT conflict with ANY section in currentCombination
- */
 function isSectionValidForSchedule(candidateSection, currentCombination) {
     // return True if candidateSection does not have conflict with currentCombination
     for (let i = 0; i < currentCombination.length; i++) {
