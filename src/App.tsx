a9f57ae0802b1d03c031f59c8fce712d7aa2a8d8
Yuan Miguel Rubio <yuanmiguelrubio@gmail.com>
2026-07-31 19:33:29 +0800

    feat: navigating the schedules

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,6 @@ function App() {
   const [selectedCourseIds, setSelectedCourseIds] = useState<string[]>([]);
   const [generatedSchedules, setGeneratedSchedules] = useState<CourseSection[][]>([]);
   const [activeScheduleIndex, setActiveScheduleIndex] = useState<number>(0);
-
-  const handleGenerate = () => {
-    alert("The button was clicked!");
-  };
-
   return (
     <div>
       <h1>🗓️ SchedKo Pathfinder</h1>
@@ -21,7 +16,26 @@ function App() {
       <p>{activeScheduleIndex + 1}</p>
 
       <button onClick={() => setGeneratedSchedules(generateSchedules(MOCK_COURSES))}>Handle Generate</button>
+      <div>
+        {/* previous button */}
+        <button
+          disabled={activeScheduleIndex === 0}
+          onClick={() => {
+            if (activeScheduleIndex > 0) {
+              setActiveScheduleIndex(activeScheduleIndex - 1)
+            }
+          }}>Previous</button>
 
+        {/*  */}
+        <span>Schedule {activeScheduleIndex + 1} of {generatedSchedules.length} </span>
+
+        {/* Next Button */}
+        <button onClick={() => {
+          if (activeScheduleIndex < (generatedSchedules.length - 1)) {
+            setActiveScheduleIndex(activeScheduleIndex + 1)
+          }
+        }}>Next</button>
+      </div>
     </div>
   )
 }
